502ade47fc15e79598af6daeda3f91a2f995f8b1
LopsanAMO <lopsan.molina@gmail.com>
2017-11-26 06:14:19 -0600

    validando la serializacion de personas

--- src/Interfaces/Serializar.java
+++ src/Interfaces/Serializar.java
@@ -13,22 +13,53 @@ import java.io.*;
  */
 public class Serializar {
     ArrayList<Persona> persona= new ArrayList<Persona>();
-    public void serializar(ArrayList<Persona> person)
-    {
+    
+    public void serializar(ArrayList<Persona> person){
         try{
-      ObjectOutputStream salida=new ObjectOutputStream(new FileOutputStream("registro.txt"));
-      this.persona= this.leerpersona();
-      salida.writeObject(this.enlazar(person, this.persona));
-      salida.close();
+            ObjectOutputStream salida=new ObjectOutputStream(new FileOutputStream("registro.txt"));
+            salida.writeObject(person);
+            salida.close();
+        }
+        catch(IOException ioe){
+            ioe.printStackTrace();
+            System.out.println(ioe);
+        }
     }
-
-    catch(IOException ioe){
-      ioe.printStackTrace();
-      System.out.println(ioe);
+    
+    public ArrayList<Persona> leerpersona(){
+        try(ObjectInputStream entrada = new ObjectInputStream(new FileInputStream("registro.txt"))) {
+            persona = (ArrayList<Persona>) entrada.readObject();
+        } 
+        catch(IOException ioe){
+            ioe.printStackTrace();
+            System.out.println(ioe);
+        }
+        catch(ClassNotFoundException c){
+           System.out.println("Class not found");
+           c.printStackTrace();
+        }
+        return persona;
     }
-    }
+    
     public ArrayList<Persona> enlazar(ArrayList<Persona> persona1, ArrayList<Persona> persona2){
         ArrayList<Persona> personaUnida = new ArrayList<Persona>();
+        
+        for(int i = 0;i < persona2.size(); i++){
+            for(int j = 0;j < persona1.size();j++){
+                if(persona2.get(i).id.equals(persona1.get(j).id)){
+                    persona2.get(i).setNombre(persona1.get(j).nombre);
+                    persona2.get(i).setApellidoPat(persona1.get(j).apellidoPaterno);
+                    persona2.get(i).setApellidoMat(persona1.get(j).apellidoMaterno);
+                    persona2.get(i).setDeuda(persona1.get(j).deuda);
+                    persona2.get(i).setCredito(persona1.get(j).credito);
+                    persona2.get(i).setDiaNac(persona1.get(j).diaNac);
+                    persona2.get(i).setMesNac(persona1.get(j).mesNac);
+                    persona2.get(i).setAnoNac(persona1.get(j).anoNac);
+                    persona1.remove(j);
+                }
+            }
+        }
+        
         for(int i=0;i< persona1.size(); i++){
             personaUnida.add(persona1.get(i));
         }
@@ -36,22 +67,5 @@ public class Serializar {
             personaUnida.add(persona2.get(i));
         }
         return personaUnida;
-    }
-    public ArrayList<Persona> leerpersona(){
-      ArrayList<Persona> personaReturn = new ArrayList<Persona>();
-      try{
-      ObjectInputStream entrada=new ObjectInputStream(new FileInputStream("registro.txt"));
-      personaReturn = (ArrayList<Persona>) entrada.readObject();
-      entrada.close();  
-        } 
-      catch(IOException ioe){
-      ioe.printStackTrace();
-      System.out.println(ioe);
-    }
-    catch(ClassNotFoundException c){
-       System.out.println("Class not found");
-       c.printStackTrace();
-    }
-      return personaReturn;
     }
 }
